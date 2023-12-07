29bb4ee3e701195fe3b43a5cff51a2e8dc983675
theprotonguy1 <152656148+theprotonguy1@users.noreply.github.com>
2023-12-07 17:45:00 +0530

    Add files via upload

--- src/app/components/Utility.jsx
+++ src/app/components/Utility.jsx
@@ -5,14 +5,14 @@ import SectionWrapper from '../hoc/SectionWrapper';
 import { fadeIn, slideIn } from '../utils/motion';
 import { styles } from '../styles';
 
-const UtilityBox = ({ bgColor, text, index }) => (
-  <motion.div
-    variants={slideIn(index % 2 === 0 ? 'left' : 'right', 'spring')}
-    className={`bg-${bgColor} md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg`}
-  >
-    {text}
-  </motion.div>
-);
+// const UtilityBox = ({ bgColor, text, index }) => (
+//   <motion.div
+//     variants={slideIn(index % 2 === 0 ? 'left' : 'right', 'spring')}
+//     className={`bg-${bgColor} md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg`}
+//   >
+//     {text}
+//   </motion.div>
+// );
 
 const Utility = () => {
   return (
@@ -26,16 +26,19 @@ const Utility = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
         <motion.div variants={slideIn('left', 'spring')} className="flex-grow">
-          <UtilityBox bgColor="yellow-200" text="Discord Subscription Bot" index={0} />
+          <h1 className='text-white border-2 border-red-500'>Discord Bot</h1>
         </motion.div>
         <motion.div variants={slideIn('right', 'spring')} className="flex-grow">
-          <UtilityBox bgColor="slate-400" text="Telegram Bot" index={1} />
+        <h1 className='text-white border-2 border-red-500'>Discord Bot</h1>
+          {/* <UtilityBox bgColor="slate-400" text="Telegram Bot" index={1} /> */}
         </motion.div>
         <motion.div variants={slideIn('left', 'spring')} className="flex-grow">
-          <UtilityBox bgColor="blue-200" text="OnlyHub Platform" index={2} />
+        <h1 className='text-white border-2 border-red-500'>Discord Bot</h1>
+          {/* <UtilityBox bgColor="blue-200" text="OnlyHub Platform" index={2} /> */}
         </motion.div>
         <motion.div variants={slideIn('right', 'spring')} className="flex-grow">
-          <UtilityBox bgColor="green-200" text="Coming Soon" index={3} />
+        <h1 className='text-white border-2 border-red-500'>Discord Bot</h1>
+          {/* <UtilityBox bgColor="green-200" text="Coming Soon" index={3} /> */}
         </motion.div>
       </div>
     </section>
