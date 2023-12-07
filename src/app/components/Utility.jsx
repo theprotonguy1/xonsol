2a6c8db50e65e200668fc67d3d21fe0f61e8bc03
theprotonguy1 <152656148+theprotonguy1@users.noreply.github.com>
2023-12-07 18:02:05 +0530

    Add files via upload

--- src/app/components/Utility.jsx
+++ src/app/components/Utility.jsx
@@ -17,28 +17,34 @@ import { styles } from '../styles';
 const Utility = () => {
   return (
     <section id="utility" className="mt-5 mb-[70px]">
-      <motion.div
-        variants={fadeIn('up', 'spring', 0.5, 0.75)}
-        className="my-8"
-      >
+      <motion.div variants={fadeIn("up", "spring", 0.5, 0.75)} className="my-8">
         <h2 className={styles.sectionHeadText}>Utilities</h2>
       </motion.div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
-        <motion.div variants={slideIn('left', 'spring')} className="bg-yellow-200 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg">
-        Discord Bot  {/* <h1 className='text-white border-2 border-red-500'>Discord Bot</h1> */}
+        <motion.div
+          variants={slideIn("left", "spring")}
+          className="bg-yellow-200 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg"
+        >
+          Discord Subscription Bot
         </motion.div>
-        <motion.div variants={slideIn('right', 'spring')} className="bg-slate-400 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg">
-        Discord Bot{/* <h1 className='text-white border-2 border-red-500'>Discord Bot</h1> */}
-          {/* <UtilityBox bgColor="slate-400" text="Telegram Bot" index={1} /> */}
+        <motion.div
+          variants={slideIn("right", "spring")}
+          className="bg-slate-400 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg"
+        >
+          Telegram Bot
         </motion.div>
-        <motion.div variants={slideIn('left', 'spring')} className="bg-blue-200 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg">
-        Discord Bot
-          {/* <UtilityBox bgColor="blue-200" text="OnlyHub Platform" index={2} /> */}
+        <motion.div
+          variants={slideIn("left", "spring")}
+          className="bg-blue-200 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg"
+        >
+          OnlyHub Platform
         </motion.div>
-        <motion.div variants={slideIn('right', 'spring')} className="bg-green-200 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg">
-        Discord Bot{/* <h1 className='text-white border-2 border-red-500'>Discord Bot</h1> */}
-          {/* <UtilityBox bgColor="green-200" text="Coming Soon" index={3} /> */}
+        <motion.div
+          variants={slideIn("right", "spring")}
+          className="bg-green-200 flex-grow md:px-12 md:py-10 px-6 py-4 md:text-lg font-bold text-black-100 h-full flex flex-col justify-center items-center rounded-lg"
+        >
+          TBA
         </motion.div>
       </div>
     </section>
